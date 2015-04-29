5ffdcd9a15ba2da14154a2907574205cbe37889e
Evan Pavlica <evan@xicreative.net>
2015-04-29 15:38:15 -0700

    Update Addon config backend for Spree 3

--- app/overrides/add_addons_config_to_admin.rb
+++ app/overrides/add_addons_config_to_admin.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(
-  virtual_path: 'spree/admin/shared/_configuration_menu',
+  virtual_path: 'spree/admin/shared/sub_menu/_configuration',
   insert_bottom: '[data-hook="admin_configurations_sidebar_menu"]',
   text: '<%= configurations_sidebar_menu_item Spree.t(:addon_configuration), admin_addons_edit_path %>',
   name: 'add_addons_config_to_admin'
---
+++ config/initializers/assets.rb
@@ -0,0 +1,1 @@
+Rails.application.config.assets.precompile += %w( spree/backend/variant_autocomplete.js )
